1ef0c7e243efb8a66fef59b365adef969554c53b
singlarajat25 <rsingla018@gmail.com>
2019-01-25 23:42:24 +0530

    Sixth Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,19 @@ if(!localStorage.getItem("hrs")){
 }else{
     var hours = parseInt(localStorage.getItem("hrs"));
 }
+
+console.log(seconds);
+console.log(minutes);
+console.log(hours);
+
+var displayhours = 0;
+var displayminutes = 0;
+var displayseconds = 0;
+var laps = [];
+var historyarr = getHistoryfromLS();
+var historyLock = 0;
+var lapLock = 0;
+
+var interval = null;
+var timerStatus = localStorage.getItem("timerStatus");
+var isHistoryVisible = "false";
